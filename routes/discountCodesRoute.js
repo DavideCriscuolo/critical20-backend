0cd5a7d83dc1c40306a3e0a0dadbabf9348a0938
Edoardo Lova <edoardolova81@gmail.com>
2025-09-05 15:44:26 +0200

    feat: implement CRUD operations for discount codes

--- routes/discountCodesRoute.js
+++ routes/discountCodesRoute.js
@@ -4,8 +4,21 @@ const discountCodesController = require("../controller/discountCodesController.j
 
 // Rotta per ottenere tutti i codici sconto
 router.get("/discoutCodes", discountCodesController.index);
+
 // Rotta per ottenere il codice sconto valido che corrisponde al code
 router.get("/discoutCodes/:code", discountCodesController.show);
+
+// Rotta per creare un nuovo codice sconto
+router.post("/discoutCodes", discountCodesController.store);
+
+// Rotta per cambiare interamente un codice sconto tramite id
+router.put("/discoutCodes/:id", discountCodesController.update);
+
+// Rotta per cambiare solo alcuni campi un codice sconto tramite id
+router.patch("/discoutCodes/:id", discountCodesController.update);
+
+// Rotta per eliminare un codice sconto tramite id
+router.delete("/discoutCodes/:id", discountCodesController.destroy);
 
 
 module.exports = router;
