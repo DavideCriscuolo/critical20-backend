7c0aad0c10794be6355ab1d7889231205971f3e5
Edoardo Lova <edoardolova81@gmail.com>
2025-09-05 12:37:34 +0200

    feat add show discount with controls

--- controller/discountCodesController.js
+++ controller/discountCodesController.js
@@ -14,7 +14,31 @@ const index = (req, res) => {
   });
 };
 
+// restituisce il singolo codice sconto verificando se è ancora valido e non è stato usato usando 'code'
+const show = (req, res) => {
+  const code = req.params.code.toUpperCase();
+  const sql= `
+    SELECT * FROM boardgames_shop.discount_codes
+    WHERE discount_codes.code = ?
+    AND valid_from <= CURDATE()
+    AND valid_to >= CURDATE()
+    AND is_used = false
+  `
+
+  connection.query(sql, [code], (err, results) => {
+    if (err) {
+      console.error("Errore durante la query:", err);
+      return res.status(500).json({ error: "Errore nel database" });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ error: "Codice sconto non trovato" });
+    }
+    res.json(results);
+  });
+}
 
 
 
-module.exports = { index };
+
+module.exports = { index, show };
