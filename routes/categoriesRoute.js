f8f5dfe7d2b040e5e4d2944987680489286875ba
Edoardo Lova <edoardolova81@gmail.com>
2025-09-06 11:18:41 +0200

    feat: add crud for categories store, update, destroy and changed show response

--- routes/categoriesRoute.js
+++ routes/categoriesRoute.js
@@ -3,4 +3,7 @@ const router = express.Router();
 const categoriesController = require("../controller/categories");
 router.get("/categories", categoriesController.index);
 router.get("/categories/:id", categoriesController.show);
+router.post("/categories", categoriesController.store);
+router.put("/categories/:id", categoriesController.update);
+router.delete("/categories/:id", categoriesController.destroy);
 module.exports = router;
